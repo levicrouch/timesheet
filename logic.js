2844b63094ebab594a076eecde9505f2912b8bb4
crhodes <connorrhodes1@yahoo.com>
2017-12-08 13:04:17 -0800

    deblah

---
+++ logic.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//* Creating the initial `.on("click")` event that will dynamically trigger new HTML rows to be generated.
+$("#submit-bid").on("click", function(){
+
+
+});

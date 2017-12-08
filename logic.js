59c8337f93945ebf43379fb7d055aa1ba3c0450e
crhodes <connorrhodes1@yahoo.com>
2017-12-08 13:07:11 -0800

    deblahhhh

--- logic.js
+++ logic.js
@@ -1,24 +1,24 @@
+/ Firebase
 
+// variable to reference firebase
 
+// variables to target HTML elements
 
+// create .on(“click”) event to trigger HTML elements
 
+   // console.log() .on(“click”) event
 
+// use .push({}) tp push employee data to the database
 
+   // console.log() the push method
 
+// use .on(“child_added”) to retrieve initial employee data from the database
 
+   // input the information retrieved into the table
 
+// use momentJS method to calculate months worked 
 
-
-
-
-
-
-
-
-
-
-
-
+// use momentJS method to calculate months billed
 
 //* Creating the initial `.on("click")` event that will dynamically trigger new HTML rows to be generated.
 $("#submit-bid").on("click", function(){

8d4a7cf8593e83b90c4de7933687b6ffc546b88f
crhodes <connorrhodes1@yahoo.com>
2017-12-08 13:27:15 -0800

    added ref().push()

--- logic.js
+++ logic.js
@@ -1,10 +1,36 @@
-/ Firebase
+// Firebase
 
 // variable to reference firebase
 
 // variables to target HTML elements
+var name = "";
+var role = "";
+var startDate = "";
+var monthsWorked = 0;
+var rate = 0;
+var total = 0;
 
 // create .on(“click”) event to trigger HTML elements
+$("#submit-bid").on("click", function () {
+    event.preventDefault();
+
+
+
+
+
+
+
+
+    database.ref().push({
+        name: name,
+        role: role,
+        startDate: startDate,
+        monthsWorked: monthsWorked,
+        rate: rate,
+        total: total
+    });
+
+});
 
    // console.log() .on(“click”) event
 
@@ -21,7 +47,3 @@
 // use momentJS method to calculate months billed
 
 //* Creating the initial `.on("click")` event that will dynamically trigger new HTML rows to be generated.
-$("#submit-bid").on("click", function(){
-
-
-});

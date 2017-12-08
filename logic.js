d2f709187550a48d8b0e1449ffeb33b5c2c38f01
crhodes <connorrhodes1@yahoo.com>
2017-12-08 13:51:48 -0800

    child_added added

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 // Firebase
 
 // variable to reference firebase
+var database = firebase.database();
 
 // variables to target HTML elements
 var name = "";
@@ -12,7 +13,6 @@ var total = 0;
 // create .on(“click”) event to trigger HTML elements
 $("#submit-bid").on("click", function(){
     event.preventDefault();
-<<<<<<< HEAD
 
 
 
@@ -35,18 +35,23 @@ $("#submit-bid").on("click", function(){
         total: total
     });
 
-=======
-       // console.log() .on(“click”) event
-    console.log(this);
-    // use .push({}) tp push employee data to the database
-    
-   // console.log() the push method
->>>>>>> 30daedfccb5df9b5f70767a8502f79ea8edd2bad
 });
 
 
 // use .on(“child_added”) to retrieve initial employee data from the database
+database.ref().on("child_added", function(childSnapshot){
 
+    
+
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().role);
+    console.log(childSnapshot.val().startDate);
+    console.log(childSnapshot.val().monthsWorked);
+    console.log(childSnapshot.val().rate);
+    console.log(childSnapshot.val().total);
+
+
+})
    // input the information retrieved into the table
 
 // use momentJS method to calculate months worked 

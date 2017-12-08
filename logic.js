f37e16037a0aaff10add825cbfe495f5b4739422
Dominic San Filippo <jaxin@Dominics-MacBook-Pro.local>
2017-12-08 13:37:08 -0800

    merge conflict

--- logic.js
+++ logic.js
@@ -9,45 +9,13 @@ var startDate = "";
 var monthsWorked = 0;
 var rate = 0;
 var total = 0;
-<<<<<<< HEAD
 // create .on(“click”) event to trigger HTML elements
 $("#submit-bid").on("click", function(){
     event.preventDefault();
        // console.log() .on(“click”) event
     console.log(this);
     // use .push({}) tp push employee data to the database
-    database.ref().push({
-
-    })
-=======
-
-// create .on(“click”) event to trigger HTML elements
-$("#submit-bid").on("click", function () {
-    event.preventDefault();
-
-
-
-
-
-
-
-
-    database.ref().push({
-        name: name,
-        role: role,
-        startDate: startDate,
-        monthsWorked: monthsWorked,
-        rate: rate,
-        total: total
-    });
-
-});
-
-   // console.log() .on(“click”) event
-
-// use .push({}) tp push employee data to the database
-
->>>>>>> 9ae77c4295091695ed369c19bed293f2d7aa5c12
+    
    // console.log() the push method
 });
 
